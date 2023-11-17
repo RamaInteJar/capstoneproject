43ae0a4892ef9c3a7160a5e05c6cf6d223571f52
pentiumKing <ramahawai87@gmail.com>
2023-11-16 19:27:05 -0500

    debugging

--- commublog/views.py
+++ commublog/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from commublog.models import Post, Comment
 from django.utils import timezone
-from django.shortcuts import render
-from commublog.models import Post, Comment
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
-from commublog.forms import PostForm, CommentForm
+from django.views.generic import (
+    TemplateView, 
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
 from django.urls import reverse_lazy
+from commublog.models import Post, Comment
+from commublog.forms import PostForm, CommentForm
 
-# Create your views here.
 class AboutView(TemplateView):
     template_name = 'about.html'
 
@@ -23,42 +27,33 @@ class PostDetailView(DetailView):
     model = Post    
 
 class CreatePostView(LoginRequiredMixin, CreateView):
-    login_required = True
-    redirect_field_name = 'commublog/post_detail.html'
-    form_class = PostForm
     model = Post
-    
+    form_class = PostForm
+    template_name = 'commublog/post_detail.html'
+    login_url = '/login/'
+
 class PostUpdateView(LoginRequiredMixin, UpdateView):
-    login_required = True
-    redirect_field_name = 'commublog/post_detail.html'
-    form_class = PostForm
     model = Post
-    
+    form_class = PostForm
+    template_name = 'commublog/post_detail.html'
+    login_url = '/login/'
+
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
 
 class DraftListView(LoginRequiredMixin, ListView):
-    login_required = True
-    redirect_field_name = 'commublog/post_list.html'
     model = Post
+    template_name = 'commublog/post_draft_list.html'
     
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('create_date')
-    
-
-
-    #########################################
-    #########################################
 
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    
     post.publish()
-    
     return redirect('post_detail', pk=pk)
-
 
 @login_required
 def add_comment_to_post(request, pk):
@@ -66,33 +61,25 @@ def add_comment_to_post(request, pk):
     
     if request.method == 'POST':
         form = CommentForm(request.POST)
-        
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            
             return redirect('post_detail', pk=post.pk)
-        
     else:
         form = CommentForm()
         
-    return render(request, 'commublog/comment_form.html', {'form':form})
+    return render(request, 'commublog/comment_form.html', {'form': form})
 
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
-    
     comment.approve()
-    
     return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
-    
     post_pk = comment.post.pk
-    
     comment.delete()
-    
     return redirect('post_detail', pk=post_pk)
